5f886db0dc3c24f539d9e3c112020731e43549d8
TaZrProdXx <102611305+TaZrProdXx@users.noreply.github.com>
2022-03-29 10:52:58 -0500

    Add files via upload

---
+++ virus/app.js
@@ -0,0 +1,8 @@
+alert("WELCOME TO THE GAME!");
+var activator = window.prompt("ENTER START TO PLAY: ");
+
+if (activator.toUpperCase() === "START"){
+    while(true){
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
+}
